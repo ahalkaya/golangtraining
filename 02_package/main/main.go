ac197cee09360e48b03be03d68087ca96746dcd2
ahalkaya <alkaya.alihan@gmail.com>
2017-09-27 00:58:01 +0300

    Add examples for packages, package scope, exported/unexported types

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ahalkaya/golangtraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Printf("%s \n", stringutil.Reverse("!erehet olleH"))
+	fmt.Printf("My name is %s \n", stringutil.MyName)
+}
